bd84cce8d401dc04d2192440c714e3291e4bc968
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-03-12 12:29:44 -0400

    Updates

---
+++ precache-manifest.8edd15ef985403e9af9156d058c8e928.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34ccedfd51015cff9753",
+    "url": "/WunderList_FrontEnd_Kawsar/static/js/runtime~main.27d62683.js"
+  },
+  {
+    "revision": "0074fc6bbb60faa27363",
+    "url": "/WunderList_FrontEnd_Kawsar/static/js/main.e288c928.chunk.js"
+  },
+  {
+    "revision": "1ed7de8f4643a91907fd",
+    "url": "/WunderList_FrontEnd_Kawsar/static/js/2.ef6d2a06.chunk.js"
+  },
+  {
+    "revision": "0074fc6bbb60faa27363",
+    "url": "/WunderList_FrontEnd_Kawsar/static/css/main.7be57966.chunk.css"
+  },
+  {
+    "revision": "670aa720e385ef6833d9ef472bd2f2b1",
+    "url": "/WunderList_FrontEnd_Kawsar/index.html"
+  }
+];
